8dd5af195b2c9d07b6f9173151c27d62851d3f59
mustofaYR <rmustofayuli@gmail.com>
2016-12-14 23:15:56 +0700

    tampilan cari dan db

--- application/controllers/Pinjam_novel.php
+++ application/controllers/Pinjam_novel.php
@@ -4,12 +4,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Pinjam_novel extends CI_Controller {
 
 	public function __construct() {
+		
         parent::__construct();
+		$this->load->model('m_novel');
     }
 	
 	public function index()
 	{
 		$this->load->view('header');
 		$this->load->view('pinjam_novel');
+	}
+	
+	public function pinjam()
+	{
+		$data['tampil'] = $this->m_novel->tampil_novel_by_id($this->uri->segment(3));
+		$this->load->view('header');
+		$this->load->view('pinjam_novel',$data);
 	}
 }
